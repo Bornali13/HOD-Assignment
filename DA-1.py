9b8bf5bad1d58b1582471e41639406c2e7d91dd1
Bornali13 <S372226@students.cdu.edu.au>
2024-10-15 13:20:18 +1100

    Create DA-1.py

---
+++ DA-1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the datasets
+df1 = pd.read_csv('C:/Users/Asus/OneDrive/Documents/GitHub/HOD-Assignment/dataset1.csv', encoding='utf-8')
+df2 = pd.read_csv('https://github.com/Bornali13/HOD-Assignment/blob/main/dataset2.csv')
+df3 = pd.read_csv('https://github.com/Bornali13/HOD-Assignment/blob/main/dataset3.csv')
