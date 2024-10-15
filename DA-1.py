95b49c2257a1fab3078e2ec9e9c9163c04be9210
Bornali13 <S372226@students.cdu.edu.au>
2024-10-15 13:33:01 +1100

    Update DA-1.py

--- DA-1.py
+++ DA-1.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load the datasets
-df1 = pd.read_csv('C:/Users/Asus/OneDrive/Documents/GitHub/HOD-Assignment/dataset1.csv', encoding='utf-8')
-df2 = pd.read_csv('https://github.com/Bornali13/HOD-Assignment/blob/main/dataset2.csv')
-df3 = pd.read_csv('https://github.com/Bornali13/HOD-Assignment/blob/main/dataset3.csv')
+chunk_size = 15000  # Adjust the chunk size as needed
+df1 = pd.read_csv('C:\Users\Asus\OneDrive\Documents\GitHub\HOD-Assignment\dataset1.csv', encoding='utf-8', chunksize=chunk_size)
+df2 = pd.read_csv('C:\Users\Asus\OneDrive\Documents\GitHub\HOD-Assignment\dataset2.csv', encoding='utf-8', chunksize=chunk_size)
+df3 = pd.read_csv('C:\Users\Asus\OneDrive\Documents\GitHub\HOD-Assignment\dataset3.csv', encoding='utf-8', chunksize=chunk_size)
+
